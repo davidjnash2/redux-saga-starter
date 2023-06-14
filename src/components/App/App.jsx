597e492f92df695887e798acde914f8b48069205
Dave Nash <davidjnash2@gmail.com>
2023-06-14 14:26:00 -0500

    updates from lecture follow along

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,35 +2,44 @@ import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import axios from 'axios';
 
+
 function App() {
   const dispatch = useDispatch();
   const elements = useSelector(store => store.elementList)
   const [newElement, setNewElement] = useState('');
 
-  const getElements = () => {
-    axios.get('/api/element').then(response => {
-      dispatch({ type: 'SET_ELEMENTS', payload: response.data });
-    })
-      .catch(error => {
-        console.log('error with element get request', error);
-      });
-  }
+  // const getElements = () => {
+  //   axios.get('/api/element').then(response => {
+  //     dispatch({ type: 'SET_ELEMENTS', payload: response.data });
+  //   })
+  //     .catch(error => {
+  //       console.log('error with element get request', error);
+  //     });
+  // }
 
   useEffect(() => {
-    getElements();
+    // getElements(),
+    dispatch({
+      type: 'FETCH_ELEMENTS'
+    })
   }, []);
 
   const addElement = () => {
-    axios.post('/api/element', { 
-      name: newElement
-    })
-      .then(() => {
-        getElements();
-        setNewElement('');
-      })
-      .catch(error => {
-        console.log('error with element get request', error);
-      });
+    // axios.post('/api/element', {
+    //   name: newElement
+    // })
+    //   .then(() => {
+    //     // getElements();
+    //     dispatch({ type: 'FETCH_ELEMENTS' })
+    //     setNewElement('');
+    //   })
+    //   .catch(error => {
+    //     console.log('error with element get request', error);
+    //   });
+
+    // payload should be object or array in dispatch
+    dispatch({ type: 'ADD_ELEMENT', payload: { name: newElement } })
+    setNewElement('');
 
   }
 
@@ -40,16 +49,16 @@ function App() {
       <h1>Atomic Elements</h1>
 
       <ul>
-        {elements.map(element => (
-          <li key={element}>
+        {elements.map((element, i )=> (
+          <li key={i}>
             {element}
           </li>
         ))}
       </ul>
 
-      <input 
-        value={newElement} 
-        onChange={evt => setNewElement(evt.target.value)} 
+      <input
+        value={newElement}
+        onChange={evt => setNewElement(evt.target.value)}
       />
       <button onClick={addElement}>Add Element</button>
     </div>
--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import createSagaMiddleware from 'redux-saga';
 import logger from 'redux-logger';
+import { takeLatest, put } from 'redux-saga/effects';
+import axios from 'axios';
+
 
 const elementList = (state = [], action) => {
     switch (action.type) {
@@ -13,11 +16,42 @@ const elementList = (state = [], action) => {
         default:
             return state;
     }
-};    
+};
+
+// saga function
+// do not usually need action as an argument for get/fetch; 
+// using it here so it can be logged for demonstration
+function* fetchElements(action) {
+    try {
+
+        console.log('firstSaga was hit with action', action);
+        // hold response from server in elementResponse
+        const elementResponse = yield axios.get('/api/element');
+        // after server response, then this enerator function/saga continues
+        yield put({ type: 'SET_ELEMENTS', payload: elementResponse.data })
+    } catch (error) {
+        console.log('error fetching elements', error)
+    }
+}
+
+// post generator functions need action argument
+function* postElement(action) {
+    try {
+        // send axios post to server with payload
+        yield axios.post('/api/element', action.payload);
+        // trigger fetchElements saga
+        yield put({ type: 'FETCH_ELEMENTS' })
+    } catch (error) {
+        console.log('error POSTing element', error);
+    }
+}
 
 // this is the saga that will watch for actions
 function* rootSaga() {
-
+    // every time an action with type 'FETCH_ELEMENTS is 
+    // dispatched, run fetchElements
+    yield takeLatest('FETCH_ELEMENTS', fetchElements);
+    yield takeLatest('ADD_ELEMENT', postElement);
 }
 
 
@@ -44,3 +78,39 @@ root.render(
         </Provider>
     </React.StrictMode>
 );
+
+
+// generator functions:
+
+// function* myGenerator() {
+//     yield true;
+//     yield 100;
+//     yield 'Oh, hello!';
+//     yield [1, 2, 3];
+// }
+
+
+// function* countDownGenerator() {
+//     let a = 10;
+//     while (a > 0) {
+//         yield `Launching in ${a}`;
+//         a -= 1;
+//     }
+//     yield `Take off!`;
+// }
+
+// let blastOff = countDownGenerator();
+
+// yield means pause the function and 
+// wait until it is called again
+// let runMyGenerator = myGenerator();
+// console.log('runMyGenerator is :', runMyGenerator);
+// console.log('next:', runMyGenerator.next().value);
+// console.log('next:', runMyGenerator.next().value);
+// console.log('next:', runMyGenerator.next().value);
+// console.log('next:', runMyGenerator.next().value);
+// console.log('next:', runMyGenerator.next().value);
+// console.log('next:', runMyGenerator.next().value);
+// console.log('next:', runMyGenerator.next().value);
+// console.log('next:', runMyGenerator.next().value);
+// console.log('next:', runMyGenerator.next().value);
